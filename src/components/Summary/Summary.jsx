96552441f4fedd28c8f313e3425f34f97005e53b
Ld-monkey <lfourteau91@gmail.com>
2023-09-08 20:16:17 +0200

    React json load data

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,51 +1,23 @@
-import IconReaction from '../../assets/images/icon-reaction.svg';
-import IconMemory from '../../assets/images/icon-memory.svg';
-import IconVerbal from '../../assets/images/icon-verbal.svg';
-import IconVisual from '../../assets/images/icon-visual.svg';
+import data from './data.json';
 import './Summary.css';
 
 function Summary() {
+  const categories = data.map((subcategory) => (
+    <li key={subcategory.category} className={`${subcategory.category.toLocaleLowerCase()}-list`}>
+      <div>
+        <img src={subcategory.icon} alt={subcategory.category} />
+        <p>{subcategory.category}</p>
+      </div>
+      <p>
+        {subcategory.score}
+        <span>/ 100</span>
+      </p>
+    </li>
+  ));
   return (
     <div className="summary-container">
       <h2>Summary</h2>
-      <ul>
-        <li className="reaction-list">
-          <div>
-            <img src={IconReaction} alt="icon reaction" />
-            <p>Reaction</p>
-          </div>
-          <p>
-            80<span>/ 100</span>
-          </p>
-        </li>
-        <li className="memory-list">
-          <div>
-            <img src={IconMemory} alt="icon memory" />
-            <p>Memory</p>
-          </div>
-          <p>
-            92<span>/ 100</span>
-          </p>
-        </li>
-        <li className="verbal-list">
-          <div>
-            <img src={IconVerbal} alt="icon verbal" />
-            <p>Verbal</p>
-          </div>
-          <p>
-            61<span>/ 100</span>
-          </p>
-        </li>
-        <li className="visual-list">
-          <div>
-            <img src={IconVisual} alt="icon visual" />
-            <p>Visual</p>
-          </div>
-          <p>
-            73<span>/ 100</span>
-          </p>
-        </li>
-      </ul>
+      <ul>{categories}</ul>
       <button type="button">Continue</button>
     </div>
   );
